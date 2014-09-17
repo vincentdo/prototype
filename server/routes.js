93e137a56c67db62ecefa1654c1ec1fe4e90a7e8
Vincent Do <do.vincent@live.com>
2014-09-17 14:22:36 -0400

    Added riot verification page

--- server/routes.js
+++ server/routes.js
@@ -17,7 +17,13 @@ module.exports = function(app) {
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
+  app.route('/riot.html')
+    .get(function(req, res) {
+      res.sendfile(app.get('appPath') + '/riot.html');
+    });
+
   // All other routes should redirect to the index.html
+  
   app.route('/*')
     .get(function(req, res) {
       res.sendfile(app.get('appPath') + '/index.html');
